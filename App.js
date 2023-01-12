e22acfd10e85f2a8f30b7dbd6e2ee8eeebaf27e1
YYTAEHWAN <ygen1002@gmail.com>
2023-01-12 10:52:22 +0900

    inital commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>자 이제 시작이야!</Text>
+      <Text>이건 진짜 어렵다. 내가 뭘 모르는지 모르겠어</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
